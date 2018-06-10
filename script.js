500af4bf56f5f41922359779c9b763ae4ddca22c
yuhanyuhan <yuhanlian@gmail.com>
2018-06-10 16:40:19 +0800

    1st Update

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ const tasks = [
 	'nail javascript',
 	'give feedback',
 ];
+
+tasks.forEach((item) => {
+	const newListItem = document.createElement('li');
+	newListItem.textContent = item;
+	document.querySelector('ul').appendChild(newListItem);
+	newListItem.addEventListener("click", function () {	
+		newListItem.classList.toggle("done")  
+
+
+		// if (newListItem.className === '') {
+		// 	newListItem.className = 'undone'
+		// }
+		// else {
+		// 	newListItem.className = 'undone'
+		// }
+		// if (newListItem.className = 'done' ||
+		// 	e.style.display == '') e.style.display = 'none';
+		// else e.style.display = 'block';
+
+	});
+})
