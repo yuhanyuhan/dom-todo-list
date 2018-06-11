23b1f7a476d4f29a0ddde17376eb6b5ab682cdc3
yuhanyuhan <yuhanlian@gmail.com>
2018-06-11 18:31:58 +0800

    final submission

--- script.js
+++ script.js
@@ -11,17 +11,32 @@ tasks.forEach((item) => {
 	document.querySelector('ul').appendChild(newListItem);
 	newListItem.addEventListener("click", function () {	
 		newListItem.classList.toggle("done")  
-
-
-		// if (newListItem.className === '') {
-		// 	newListItem.className = 'undone'
-		// }
-		// else {
-		// 	newListItem.className = 'undone'
-		// }
-		// if (newListItem.className = 'done' ||
-		// 	e.style.display == '') e.style.display = 'none';
-		// else e.style.display = 'block';
-
 	});
 })
+
+const button = document.querySelector("#add-todo")
+
+button.addEventListener("click", function(event) {
+	if (document.querySelector('#item').value !== '') {
+	var item = document.querySelector('#item').value
+	const newLi = document.createElement('li')
+	newLi.textContent = item 
+	document.querySelector('ul').appendChild(newLi)
+	}
+})
+
+const input = document.querySelector("#item")
+
+input.addEventListener("keypress", function(event) {
+	if (document.querySelector('#item').value !== '' && event.charCode ===13 ) {
+	var item = document.querySelector('#item').value
+	const newLi = document.createElement('li')
+	newLi.textContent = item 
+	document.querySelector('ul').appendChild(newLi)
+	}
+})
+
+
+
+
+
